7bfde9949a29dfcdbe51d6d65f101a9a86299f72
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2022-03-29 22:22:52 +0530

    Get Method to show all the Pins

--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -15,6 +15,16 @@ router.post("/", async (req, res) => {
 
 //Get All Pins 
 
+router.get("/", async (req, res) => {
+    try {
+        const pins = await Pin.find();
+        res.status(200).json(pins);
+        
+    } catch (err) {
+        res.status(500).json(err)        
+    }
+});
+
 
 module.exports = router;
 
