7ada0dcb226c1d1294065bd3ffd71e751f7f46e8
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2022-04-17 12:53:00 +0530

    Mapbox Integration
    https://www.mapbox.com/maps
    Go to account and copy the token and use that in the .env file in the front end.
    Install react-map-gl mapbox-gl
    refer documentation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,32 @@
+import * as React from 'react';
+import Map, {Marker, Popup} from 'react-map-gl';
+
 
 function App() {
+  // const [showPopup, setShowPopup] = React.useState(true);
+
   return (
     <div className="App">
-      Hello React
+      <Map
+      initialViewState={{
+        latitude: 20.59,
+        longitude: 78.9,
+        zoom: 4
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+    >
+      <Marker longitude={80.27} latitude={13.08} color="red" />
+      <Popup longitude={80.27} latitude={13.08}
+        anchor="left"
+        // closeButton={true}
+        // closeOnClick={true}
+        // onClose={() => setShowPopup(false)}
+        >
+        You are here
+      </Popup>
+    </Map>
     </div>
   );
 }
